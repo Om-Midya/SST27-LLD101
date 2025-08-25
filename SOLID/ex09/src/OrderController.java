8a56bbb433d9a147d5dc6f250068402435213663
Om-Midya <dukemidya7521@gmail.com>
2025-08-25 11:07:30 +0530

    Refactor OrderController to use dependency injection and introduce OrderRepository interface, ex09 done

--- SOLID/ex09/src/OrderController.java
+++ SOLID/ex09/src/OrderController.java
@@ -1,6 +1,10 @@
 public class OrderController {
+    OrderRepository repo;
+
+    OrderController(OrderRepository repo){
+        this.repo = repo;
+    }
     void create(String id){
-        SqlOrderRepository repo = new SqlOrderRepository(); // hard dependency
         repo.save(id);
         System.out.println("Created order: " + id);
     }
