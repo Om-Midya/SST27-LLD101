67979ef9080ad163176c100587cf7c4026a5ad87
Om-Midya <dukemidya7521@gmail.com>
2025-09-08 11:37:48 +0530

    Implement TextStyle and TextStyleFactory to optimize text rendering with flyweight pattern

--- design-pattern-assignments/flyweight-glyphs/src/com/example/render/App.java
+++ design-pattern-assignments/flyweight-glyphs/src/com/example/render/App.java
@@ -2,6 +2,7 @@ package com.example.render;
 public class App {
     public static void main(String[] args) {
         Renderer r = new Renderer();
-        System.out.println("Cost=" + r.render("Hello Flyweight! ".repeat(2000)));
+        String text = "Hello Flyweight! ".repeat(2000);
+        System.out.println("Cost=" + r.render(text));
     }
 }
